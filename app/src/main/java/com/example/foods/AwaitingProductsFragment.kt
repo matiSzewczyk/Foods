ec0c53736ec1b5285d6bb334e2f8206e0f4d4131
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-03-06 20:56:24 +0100

    new icon

--- app/src/main/java/com/example/foods/AwaitingProductsFragment.kt
+++ app/src/main/java/com/example/foods/AwaitingProductsFragment.kt
@@ -3,6 +3,7 @@ package com.example.foods
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -58,6 +59,7 @@ class AwaitingProductsFragment : Fragment(R.layout.fragment_awaiting_products), 
                                     .setContentTitle(awaitingProductsViewModel.notifyObjectName())
                                     .setContentText(awaitingProductsViewModel.notifyObjectGrammage())
                                     .setSmallIcon(R.drawable.foods_icon)
+                                    .setLargeIcon(BitmapFactory.decodeResource(requireContext().resources, R.mipmap.logo_round))
                                     .setColor(resources.getColor(R.color.green_900))
                                     .setPriority(NotificationCompat.PRIORITY_MAX).build()
 
