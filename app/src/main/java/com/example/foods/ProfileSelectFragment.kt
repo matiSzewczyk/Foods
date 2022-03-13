3b828f8c75575f2de3ef30c5c641c25f019a5ab7
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-03-13 21:11:58 +0100

    Change margin values

--- app/src/main/java/com/example/foods/ProfileSelectFragment.kt
+++ app/src/main/java/com/example/foods/ProfileSelectFragment.kt
@@ -3,6 +3,7 @@ package com.example.foods
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.foods.databinding.FragmentProfileSelectBinding
